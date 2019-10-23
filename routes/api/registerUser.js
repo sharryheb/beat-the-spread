25d3f33b475af38e41a12bcba611317ee62d1470
Matt Jennings <matt@mattjennings.net>
2019-10-22 21:03:32 -0700

    added route and controller for registerUser route

---
+++ routes/api/registerUser.js
@@ -0,0 +1,35 @@
+const User = require('../../models/user');
+const passport = require('passport');
+
+const router = require("express").Router();
+
+router.post('/api/registerUser', (req, res, next) => {
+  passport.authenticate('register', (err, user, info) => {
+    if(err) {
+      console.log(err);
+    }
+    if(info !== undefined) {
+      console.log(info.message);
+      res.send(info.message);
+    } else {
+      req.logIn(user, err => {
+        const data = {
+          username: req.body.username,
+          email: req.body.email,
+          avatar: req.body.avatar,
+          favoriteTeamId: req.body.favoriteTeamId,
+        };
+        User.findOne({
+          where: {
+            username: data.username
+          },
+        }).then(() => {
+          console.log('user created in db');
+          res.status(200).send({ message: 'user created' });
+        });
+      });
+    }
+  })(req, res, next);
+});
+
+module.exports = router;
