314e5fcedb530ace77f9bfc214724168b98a2053
Matt Jennings <matt@mattjennings.net>
2019-10-28 21:30:37 -0700

    got logout working

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const registerUserRoute = require('./registerUser');
 const loginUserRoute = require('./loginUser');
 const profileRoute = require('./profile');
+const logOutRoute = require('./logout');
 
 router.use(registerUserRoute);
 router.use(loginUserRoute);
 router.use(profileRoute);
+router.use(logOutRoute);
 
 module.exports = router;
