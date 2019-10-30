a9520ba1cd6499c0d248e1f689e494546c20e97d
Suzanne Cho <suzcho@uw.edu>
2019-10-30 01:49:56 -0700

    UI-updates

--- client/src/components/FanStanding/index.js
+++ client/src/components/FanStanding/index.js
@@ -5,66 +5,69 @@ import { Card, ListGroup, ListGroupItem, Table } from 'react-bootstrap';
 import "./style.css";
 
 import Avatar from "../Avatar";
-
 function FanStanding() {
-    return (
-        <Card className="fs" style={{ width: '40rem' }}>
-        {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
-        <Card.Body>
-          <Card.Title>Fan Standing</Card.Title>
-          {/* <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text> */}
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem> <Table>
-            <tbody>
-              <tr>
-                <td>
-                  <Avatar />
-                </td>
-                <td>
-                <Card.Link href="#">Star 123</Card.Link>
-                <Card.Text>14 correct predictions <br></br>Favorite Sports Team: San Francisco 49ers</Card.Text>
-                </td>
-              </tr>
-            </tbody>
-            </Table>
+  return (
+      <Card className="fs" style={{ width: '40rem' }}>
+      {/* <Card.Img variant="top" src="holder.js/100px180?text=Image cap" /> */}
+      <Card.Body>
+        <Card.Title>Fan Standing</Card.Title>
+        {/* <Card.Text>
+          Some quick example text to build on the card title and make up the bulk of
+          the card's content.
+        </Card.Text> */}
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem> <Table>
+          <tbody>
+            <tr>
+              <td>
+                <Avatar />
+              </td>
+              <td>
               
-          </ListGroupItem>
-          <ListGroupItem>
-            <Table>
-            <tbody>
-              <tr>
-                <td>
-                  <Avatar />
-                </td>
-                <td>
-                  Sarah Zhou
-                </td>
-              </tr>
-            </tbody>
-            </Table>
-          </ListGroupItem>
-          <ListGroupItem>
+              <i><b>Star123</b></i>
+              <Card.Text>14 correct predictions <br></br>Favorite Sports Team: San Francisco 49ers</Card.Text>
+              </td>
+            </tr>
+          </tbody>
+          </Table>
+            
+        </ListGroupItem>
+        <ListGroupItem>
           <Table>
-            <tbody>
-              <tr>
-                <td>
-                  <Avatar />
-                </td>
-                <td>
-                 The Notorious M.P.J.
-                </td>
-              </tr>
-            </tbody>
-            </Table>
-            </ListGroupItem>
-        </ListGroup>
-        
-      </Card>
-    );
-  }
-  
-  export default FanStanding;
+          <tbody>
+            <tr>
+              <td>
+                <Avatar />
+              </td>
+              <td>
+               
+              <i><b>Sarah Zhou</b></i>
+                <Card.Text>13 correct predictions <br></br>Favorite Sports Team: Carolina Panthers</Card.Text>
+              </td>
+            </tr>
+          </tbody>
+          </Table>
+        </ListGroupItem>
+        <ListGroupItem>
+        <Table>
+          <tbody>
+            <tr>
+              <td>
+                <Avatar />
+              </td>
+              <td>
+               <i><b>The Notorious M.P.J.</b></i>
+               <Card.Text>1 correct prediction<br></br>Favorite Sports Team: Seattle Seahawks</Card.Text>
+              </td>
+            </tr>
+          </tbody>
+          </Table>
+          </ListGroupItem>
+      </ListGroup>
+      
+    </Card>
+  );
+}
+
+export default FanStanding;
--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -13,16 +13,24 @@ function About() {
     
      
       <div className="About">
-
+         
        <Navme />
+       
        <h2> About Us</h2>
-       <textarea >
-        We are an online football betting site that allows fans to gather, bet and have fun!
+       <img src="/images/football.jpg" width="700px" 
+       ></img>
+       
+       <textarea>We are an online football betting site that allows fans to gather, bet and have fun!
+
+   Enhance your game watching experience to another level by seeing if you are a true fan who knows how to predict a future win!
+
+Goal : Compete with friends by predicting which NFL teams will meet the point spread, or beat the point spread. Use real NFL point spreads in this fake "gambling" game.
+
        </textarea>
-       
-       
+  
        
       </div>
+
     
   );
 }
