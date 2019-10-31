c6b92ef05ea6ea1357aa1be84e0a96a95ff9416c
Matt Jennings <matt@mattjennings.net>
2019-10-30 18:37:51 -0700

    removed unneeded config/config.js

--- config/config.js
+++ config/config.js
@@ -21,28 +21,3 @@ let config = {
 };
 
 module.exports = config;
-
-// {
-//   "development": {
-//     "username": "root",
-//     "password": "password",
-//     "database": "beat_the_spread",
-//     "host": "127.0.0.1",
-//     "port": 3306,
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
