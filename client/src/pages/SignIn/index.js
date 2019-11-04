2950136aa709fe420e9d14bb6681d0cfff473d4d
Matt Jennings <matt@mattjennings.net>
2019-11-03 21:00:54 -0800

    start of SignIn page code

--- client/src/pages/SignIn/index.js
+++ client/src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Navme from "../../components/Nav";
 
@@ -8,53 +8,112 @@ import authAPI from "../../utils/authAPI";
 
 import "./style.css";
 
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
 
 // MATT:
 // I have no idea if the following method (onSubmit) works, but this should be how you
 // login a user - should call "loginUser" on the "authAPI" client-side file,
 // which in turn will make an axios call to our server. look at the signUp to
 // see what I mean - I fixed that one up to be how it should work. Let me know if ??? -sharry
-const onSubmit = (e) => {
-    e.preventDefault();
-    authAPI.loginUser();
-}
+class SignIn extends Component {
+    state = {
+        email: ''
+    };
+    
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
-function SignIn() {
-  return (
+    onSubmit = event => {
+        event.preventDefault();
+        authAPI.loginUser();
+    }
 
-      <div className="SignIn">
-       <Navme />
-       <p id="sign">Sign In</p>
-       <Form onSubmit={onSubmit} style={{ width: '18rem' }}>
-            <Form.Group controlId="formBasicEmail">
+    render() {
+        return (
 
-                <Form.Control type="text" placeholder="Username" />
+            <div className="SignIn">
+            <Navme />
+            <p id="sign">Sign In</p>
+            <Form style={{ width: '18rem' }}>
+                    <Form.Group controlId="formBasicEmail">
 
-            </Form.Group>
+                        <Form.Control type="text" placeholder="Email" name="email" />
 
-            <Form.Group controlId="formBasicPassword">
-
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Row>
-                <Col>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember Me" />
                     </Form.Group>
-                </Col>
-                <Col>
 
-                    <Button variant="primary" type="submit">
-                        Sign in
-                    </Button>
-                </Col>
-            </Row>
-       </Form>
+                    <Form.Group controlId="formBasicPassword">
+
+                        <Form.Control type="password" placeholder="Password" name="password" />
+                    </Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Remember Me" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+
+                            <Button variant="primary" type="submit" onClick={this.onSubmit}>
+                                Sign in
+                            </Button>
+                        </Col>
+                    </Row>
+            </Form>
 
 
-      </div>
+            </div>
 
-  );
+        );        
+    }
 }
 
 export default SignIn;
+
+
+// const onSubmit = (e) => {
+//     e.preventDefault();
+//     authAPI.loginUser();
+// }
+
+// function SignIn() {
+//   return (
+
+//       <div className="SignIn">
+//        <Navme />
+//        <p id="sign">Sign In</p>
+//        <Form onSubmit={onSubmit} style={{ width: '18rem' }}>
+//             <Form.Group controlId="formBasicEmail">
+
+//                 <Form.Control type="text" placeholder="Email" name="email" />
+
+//             </Form.Group>
+
+//             <Form.Group controlId="formBasicPassword">
+
+//                 <Form.Control type="password" placeholder="Password" name="password" />
+//             </Form.Group>
+//             <Row>
+//                 <Col>
+//                     <Form.Group controlId="formBasicCheckbox">
+//                         <Form.Check type="checkbox" label="Remember Me" />
+//                     </Form.Group>
+//                 </Col>
+//                 <Col>
+
+//                     <Button variant="primary" type="submit">
+//                         Sign in
+//                     </Button>
+//                 </Col>
+//             </Row>
+//        </Form>
+
+
+//       </div>
+
+//   );
+// }
