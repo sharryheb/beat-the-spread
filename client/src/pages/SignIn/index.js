1f3ff24af2299189f7b52d4ce3ba91adba8ea45a
Matt Jennings <matt@mattjennings.net>
2019-11-05 22:58:37 -0800

    deleted unneeded code on SignIn/index.js

--- client/src/pages/SignIn/index.js
+++ client/src/pages/SignIn/index.js
@@ -4,8 +4,6 @@ import Navme from "../../components/Nav";
 
 import { Form, Button, Row, Col } from 'react-bootstrap';
 
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
 import authAPI from "../../utils/authAPI";
 
 import "./style.css";
@@ -40,18 +38,6 @@ class SignIn extends Component {
                 let logInSuccessOrErrorMsgCookieObj = cookies.get('logInSuccessOrErrorMsg');
                 console.log(logInSuccessOrErrorMsgCookieObj);
                 if(response.data && logInSuccessOrErrorMsgCookieObj.success) {
-                    // this.setState({
-                    //     errorOrSuccessMsg: {
-                    //         successMsg: logInSuccessOrErrorMsgCookieObj.success.message,
-                    //         userInfo: {
-                    //             email: logInSuccessOrErrorMsgCookieObj.success.email,
-                    //             screename: logInSuccessOrErrorMsgCookieObj.success.screenname,
-                    //             avatar: logInSuccessOrErrorMsgCookieObj.success.avatar,
-                    //             favoriteTeamCode: logInSuccessOrErrorMsgCookieObj.favoriteTeamCode
-                    //         }
-                    //     },
-                    //     password: ''
-                    // });
                     this.props.history.push('/');
                 } else {
                     this.setState({
