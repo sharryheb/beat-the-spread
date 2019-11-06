2a19c756850a9d3cd1df0f379f4bf0e02bc52448
Matt Jennings <matt@mattjennings.net>
2019-11-06 00:26:30 -0800

    added logout button

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,6 +3,20 @@ import { Nav } from 'react-bootstrap';
 
 import "./style.css";
 
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+const logInSuccessOrErrorMsgCookieObj = cookies.get('logInSuccessOrErrorMsg');
+let logOutNav;
+
+if(logInSuccessOrErrorMsgCookieObj.success) {
+  logOutNav = (
+    <Nav.Item as="li">
+      <Nav.Link className="topitems" href="/SignOut">Log Out</Nav.Link>
+    </Nav.Item>
+  );
+}
+
 function Navme() {
     return (
       <Nav className="topbar" defaultActiveKey="/" as="ul">
@@ -15,6 +29,7 @@ function Navme() {
         <Nav.Item as="li">
           <Nav.Link className="topitems" href="/SignIn">Log In</Nav.Link>
         </Nav.Item>
+        {logOutNav}
         <Nav.Item as="li">
           <Nav.Link className="topitems" href="/about">About</Nav.Link>
         </Nav.Item>
