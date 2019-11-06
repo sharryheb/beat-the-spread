857efbb02d347230fefdc62cea4c51a5f6696460
sharryheb <43225557+sharryheb@users.noreply.github.com>
2019-11-06 12:47:55 -0800

    fixing icons

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-
 import { Nav } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { FaFootballBall} from 'react-icons/fa';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faYoutube, faFacebook, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons";
 import "./style.css";
 import authAPI from "../../utils/authAPI";
-
 
 function logOut(event) {
   event.preventDefault();
@@ -15,7 +17,7 @@ function Navme() {
     return (
       <Nav className="topbar" defaultActiveKey="/" as="ul">
         <Nav.Item as="li">
-          <Nav.Link className="topitems">Beat-the-Spread</Nav.Link>
+          <Nav.Link className="topitems">Beat-the-Spread<FaFootballBall/></Nav.Link>
         </Nav.Item>
         <Nav.Item as="li">
           <Nav.Link className="topitems" href="/">Home</Nav.Link>
@@ -29,6 +31,30 @@ function Navme() {
         <Nav.Item as="li">
           <Nav.Link className="topitems" href="/about">About</Nav.Link>
         </Nav.Item>
+    <div>
+   <a
+    href="https://www.youtube.com/beat-the-spread"
+    className="youtube social"
+ >YouTube
+    <FontAwesomeIcon icon={faYoutube} size="2x" />
+ </a>
+ <a
+  href="https://www.facebook.com/beat-the-spread"
+  className="facebook social"
+>Facebook
+  <FontAwesomeIcon icon={faFacebook} size="2x" />
+</a>
+<a href="https://www.twitter.com/beat-the-spread" className="twitter social">
+  Twitter <FontAwesomeIcon icon={faTwitter} size="2x" />
+</a>
+<a
+  href="https://www.instagram.com/beat-the-spread"
+  className="instagram social"
+>Instagram
+  <FontAwesomeIcon icon={faInstagram} size="2x" />
+</a>
+    </div>
+
       </Nav>
     );
   }
--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -6,8 +6,6 @@ import "./style.css";
 
 import Navme from "../../components/Nav";
 
-
-
 function About() {
   return (
     
@@ -17,12 +15,13 @@ function About() {
        <Navme />
        
        <h2> About Us</h2>
-       <textarea>We are an online football betting site that allows fans to gather, bet and have fun!
+      <textarea>
+      We are an online football betting site that allows fans to gather, bet and have fun!
+      
+       Enhance your game watching experience to another level by seeing if you are a true fan who knows how to predict a future win!
 
-   Enhance your game watching experience to another level by seeing if you are a true fan who knows how to predict a future win!
 
 Goal : Compete with friends by predicting which NFL teams will meet the point spread, or beat the point spread. Use real NFL point spreads in this fake "gambling" game.
-
        </textarea>
   
        
