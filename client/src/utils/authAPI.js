fae3c16f1a36a45aabfd53a8b22e5b148b67bbb9
Matt Jennings <matt@mattjennings.net>
2019-11-06 00:35:23 -0800

    created logOut method inside authAPI.js client file

--- client/src/utils/authAPI.js
+++ client/src/utils/authAPI.js
@@ -5,7 +5,9 @@ export default {
     loginUser: function(authData) {
         return axios.post("/api/auth/loginUser", authData);
     },
-
+    logOut: function() {
+        return axios.get("/api/auth/logoutUser");
+    },
     registerUser: function(authData) {
         return axios.post("/api/auth/registerUser", authData);
     }
